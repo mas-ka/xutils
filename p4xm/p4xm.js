3c940d255506fc515155e101c180a4adab186a08
mas-ka <masahide.kawamoto@gmail.com>
2016-10-28 10:53:09 +0900

    k=20まで実装、formatFloat実装

--- p4xm/p4xm.js
+++ p4xm/p4xm.js
@@ -48,56 +48,59 @@ app.controller('myController', function(numberFilter){
     Math.toRadians = function(degree){
         return isNaN(degree)?NaN:degree*Math.PI/180;
     }
+    Math.formatFloat = function(f, s){
+        var z = Array(s+1).join("0");
+        var t = ""+f+(((""+f).indexOf('.')<0)?"."+z:z);
+        return parseFloat((t.split('\.'))[0]+"."+(t.split('\.'))[1].substr(0,s));
+    }
 
-    this.energy2theta = function(e) { return 0.00001*Math.round(100000*Math.toDegrees(Math.asin(this.EL/(2*this.xtal.d*e)))); }
+    this.energy2theta = function(e) { return Math.toDegrees(Math.asin(this.EL/(2*this.xtal.d*e))); }
     this.theta2energy = function(t) { return this.EL/(2*this.xtal.d*Math.sin(Math.toRadians(t))); }
     this.k2energy = function(k, e0) { return e0 + k*k / 0.262467191; }
     this.energy2k = function(e, e0) { return Math.sqrt(0.262467191 * (e - e0)); }
 
-
-
     this.updateAllThetas = function() {
         var E0 = this.AbsEnergy;
-        this.thetas[0] = this.energy2theta(E0-330); // Measure Start
-        this.thetas[1] = this.energy2theta(E0- 30); // XANES Start
+        this.thetas[0] = Math.formatFloat(this.energy2theta(E0-330), 5); // Measure Start
+        this.thetas[1] = Math.formatFloat(this.energy2theta(E0- 30), 5); // XANES Start
         for (i = 2, k = 4 ; i <= 10 ; i++, k+=2)
-            this.thetas[i] = this.energy2theta(this.k2energy(k, E0));
+            this.thetas[i] = Math.formatFloat(this.energy2theta(this.k2energy(k, E0)), 5);
         this.divs = [ 50, 250,  40,  40,  40,  40,  40,  40,  40,  40];
         for (i = 0 ; i < 10 ; i++) {
             var be = this.thetas[i] - this.thetas[i+1];
             var n = this.divs[i];
-            this.steps[i] = 0.00001*Math.round(100000*be/n)||0.00001;
+            this.steps[i] = Math.formatFloat((0.00001*Math.round(100000*be/n))||0.00001, 5);
             this.divs[i] = parseInt(Math.round(be/this.steps[i]));
         }
         for (i = 0 ; i <= 10 ; i++)
-            this.energies[i] = Math.round(100*this.theta2energy(this.thetas[i]))/100.0;
+            this.energies[i] = Math.formatFloat(this.theta2energy(this.thetas[i]), 2);
     }
 
     this.updateEnergy = function(idx) {
-        this.thetas[idx] = this.energy2theta(this.energies[idx]);
+        this.thetas[idx] = Math.formatFloat(this.energy2theta(this.energies[idx]), 5);
         if (idx == 0) this.divs[0] = parseInt(Math.round((this.thetas[0]-this.thetas[1])/this.steps[0]));
         else if (idx == 10) this.divs[idx-1] = parseInt(Math.round((this.thetas[idx-1]-this.thetas[idx])/this.steps[idx-1]));
         else {
             this.divs[idx-1] = parseInt(Math.round((this.thetas[idx-1]-this.thetas[idx])/this.steps[idx-1]));
             this.divs[idx] = parseInt(Math.round((this.thetas[idx]-this.thetas[idx+1])/this.steps[idx]));
         }
-        if (idx > 1) this.ks[idx] = 0.00001 * Math.round(100000 * this.energy2k(this.energies[idx], this.AbsEnergy));
+        if (idx > 1) this.ks[idx] = Math.formatFloat(this.energy2k(this.energies[idx], this.AbsEnergy), 5);
     }
 
     this.updateTheta = function(idx) {
-        this.energies[idx] = 0.01 * Math.round(100 * this.theta2energy(this.thetas[idx]));
+        this.energies[idx] = Math.formatFloat(this.theta2energy(this.thetas[idx]), 2);
         if (idx == 0) this.divs[0] = parseInt(Math.round((this.thetas[0]-this.thetas[1])/this.steps[0]));
         else if (idx == 10) this.divs[idx-1] = parseInt(Math.round((this.thetas[idx-1]-this.thetas[idx])/this.steps[idx-1]));
         else {
             this.divs[idx-1] = parseInt(Math.round((this.thetas[idx-1]-this.thetas[idx])/this.steps[idx-1]));
             this.divs[idx] = parseInt(Math.round((this.thetas[idx]-this.thetas[idx+1])/this.steps[idx]));
         }
-        if (idx > 1) this.ks[idx] = 0.00001 * Math.round(100000 * this.energy2k(this.energies[idx], this.AbsEnergy));
+        if (idx > 1) this.ks[idx] = Math.formatFloat(this.energy2k(this.energies[idx], this.AbsEnergy), 5);
     }
 
     this.updateK = function(idx) {
-        this.energies[idx] = 0.01 * Math.round(100 * this.k2energy(this.ks[idx], this.AbsEnergy));
-        this.thetas[idx] = this.energy2theta(this.energies[idx]);
+        this.energies[idx] = Math.formatFloat(this.k2energy(this.ks[idx], this.AbsEnergy), 2);
+        this.thetas[idx] = Math.formatFloat(this.energy2theta(this.energies[idx]), 5);
         if (idx == 0) this.divs[0] = parseInt(Math.round((this.thetas[0]-this.thetas[1])/this.steps[0]));
         else if (idx == 10) this.divs[idx-1] = parseInt(Math.round((this.thetas[idx-1]-this.thetas[idx])/this.steps[idx-1]));
         else {
@@ -109,7 +112,7 @@ app.controller('myController', function(numberFilter){
     this.updateDiv = function(idx) {
         var be = this.thetas[idx] - this.thetas[idx+1];
         var n = this.divs[idx];
-        this.steps[idx] = 0.00001 * Math.round(100000 * be/n)||0.00001;
+        this.steps[idx] = Math.formatFloat((0.00001 * Math.round(100000 * be/n))||0.00001, 5);
         this.divs[idx] = parseInt(Math.round(be/this.steps[idx]));
     }
 
