5134ea7da1b0b4b6a78ea8c9ae9eae6babb91a8c
mas-ka <masahide.kawamoto@gmail.com>
2026-05-27 10:44:36 +0900

    P4XM[SQ}の出力するAgendaにモノクロメータ結晶の情報を追加した(※この情報はXUIM3では無視される)

--- p4xmq/p4xmq.js
+++ p4xmq/p4xmq.js
@@ -14,6 +14,13 @@ app.controller('myController', function($resource, $mdDialog, numberFilter){
         this.isNotIntrinsicPlane = (this.xtal.name=="Other...");
         this.applyAbsEnergy();
     }
+    this.getXtalPlaneName = function() {
+        if (this.isNotIntrinsicPlane) {
+            return "UNKNOWN";
+        } else {
+            return (this.xtal.name).toUpperCase();
+        }
+    }
 
     this.ElementNames = getElementNames();
     this.element_name = "Cu";
@@ -276,6 +283,10 @@ app.controller('myController', function($resource, $mdDialog, numberFilter){
     this.createText4SagaAgenda = function() {
         var l = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\r\n";
         l += "<parameter>\r\n";
+        l += "  <monochrometer>\r\n";
+        l += "    <d_spacing unit=\"angstrom\">"+String.formatF(this.xtal.d, 10, 6).trim()+"</d_spacing>\r\n";
+        l += "    <name>"+this.getXtalPlaneName().trim()+"</name>\r\n";
+        l += "  </monochrometer>\r\n";
         l += "  <element>\r\n";
         l += "    <symbol>"+this.element_name+"</symbol>\r\n";
         l += "    <edge>"+this.edge+"</edge>\r\n";
