7faa31ad8df7e1b339a3c18ecb52a4e25e8426db
mas-ka <masahide.kawamoto@gmail.com>
2026-05-29 12:30:30 +0900

    P4XMQにもパラメータ値のバリデーションを実装した

--- p4xms/p4xms.js
+++ p4xms/p4xms.js
@@ -110,10 +110,11 @@ app.controller('myController', function($resource, $mdDialog, numberFilter){
         this.divs[this.block-1]++;
     }
 
-    this.changeBlock = function() {
-        this.divs[this.block_prev-1]--;
-        this.divs[this.block-1]++;
+    this.changeBlock = function() { // プルダウンでBlock数が変更されたら露光時間以外のパラメータを初期値に戻す
         this.block_prev = this.block;
+        this.ks = [0, 0, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+        this.divs = [ 50, 250,  40,  40,  40,  40,  40,  40,  40,  41];
+        this.updateAllThetas();
         for (i = 0 ; i < this.block ; i++) this.block_shows[i] = true;
         for (i = this.block ; i < 10 ; i++) this.block_shows[i] = false;
     }
