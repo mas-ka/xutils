a127e6a1bdb6bff5333a0f34620ffdce5a2679c8
mas-ka <masahide.kawamoto@gmail.com>
2026-08-02 03:36:54 +0900

    レンジ計算と表示を仕上げた。ナビから呼び出されるもの新しいVue版に変更した

--- calq2/calq2.js
+++ calq2/calq2.js
@@ -44,6 +44,26 @@ createApp({
         // ローカルの宣言
 
         // ユーティリティ関数
+        const formatF = function (f, w, d) {
+            var z = Array(d + 1).join("0");
+            var y = "" + f + ((("" + f).indexOf('.') < 0) ? "." + z : z);
+            var t = "." + (y.split('\.'))[1].substr(0, d);
+            if (Math.abs(f) < 1) {
+                return '0' + Array(w - (y.split('\.'))[0].length - t.length + 2).join(" ") + (f < 0 ? "-" : "") + t;
+            } else {
+                return Array(w - (y.split('\.'))[0].length - t.length + 1).join(" ") + (y.split('\.'))[0] + t;
+            }
+        }
+
+        const toDegrees = function (radian) {
+            var toDegree = 180 / Math.PI;
+            if (isNaN(radian)) return NaN;
+            return radian * toDegree;
+        }
+
+        const calcTheta = function (R, L) {
+            return 0.5 * Math.atan2(R, L);
+        }
 
         // 初期化関数
 
@@ -57,12 +77,35 @@ createApp({
             }
         })
 
+        const calcRanges = function (mode, diameter, length) {
+            let Tin = calcTheta(diameter / 2, length);
+            let Tout = calcTheta(radius.value, length);
+            let Qin = 4 * Math.PI * Math.sin(Tin) / lambda.value * 10;
+            let Qout = 4 * Math.PI * Math.sin(Tout) / lambda.value * 10;
+            let Din = 2 * Math.PI / Qin;
+            let Dout = 2 * Math.PI / Qout;
+            let Sin = 2 * Math.sin(Tin) / lambda.value * 10;
+            let Sout = 2 * Math.sin(Tout) / lambda.value * 10;
+            switch (mode) {
+                case 0: // 2θ
+                    return formatF(toDegrees(2 * Tin), 5, 2) + " - " + formatF(toDegrees(2 * Tout), 5, 2);
+                case 1: // d
+                    return formatF(Din, 5, 2) + " - " + formatF(Dout, 5, 2);
+                case 3: // s
+                    return formatF(Sin, 7, 4) + " - " + formatF(Sout, 7, 4);
+                case 2: // q
+                default:
+                    // return formatF(Qin, 7, 4) + " - " + formatF(Qout, 7, 4);
+                    return sprintf("%7.4f - %7.4f", Qin, Qout);
+            }
+        }
 
 
         return {
             licenseDialog, licenseContent,
             lambda, Unit_lambda, xrayValue,
             radius, Modes, Mode, lengths, BSdiameters,
+            calcRanges,
         }
     }
 }).use(createVuetify()).mount('#app')
